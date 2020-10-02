2b7f2cc70f48f7e0e5dfe0f71462db7d3957f0dd
udayanga <udayanga.amarasena@dimolanka.com>
2020-10-02 16:36:37 +0530

    Added Contact Form 7 plugin

--- wp-content/themes/blocksy-child/functions.php
+++ wp-content/themes/blocksy-child/functions.php
@@ -5,3 +5,17 @@ if (! defined('WP_DEBUG')) {
 add_action( 'wp_enqueue_scripts', function () {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 });
+
+
+//SMPT Setup
+function my_phpmailer_setup( PHPMailer $phpmailer ) {
+// 	$phpmailer->Host = 'smtp-relay.gmail.com';
+// 	$phpmailer->Port = 587;
+// 	$phpmailer->SMTPAuth = true;
+// 	$phpmailer->Username = 'username';
+// 	$phpmailer->Password = 'password';
+	
+	$phpmailer->isSMTP();
+	
+	//Reference: https://github.com/PHPMailer/PHPMailer
+}
